902e6feb5c9ac4ec01fe3daae8f69f37cf484783
Baev Ivan <ivan.baev.14@gmail.com>
2018-11-23 01:25:06 +0300

    Fixing .env

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,9 @@ import config from './config';
 import runSockets from './socket';
 import {Routes} from './routes';
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express();
 const server = http.createServer(app);
 const routes = new Routes();
